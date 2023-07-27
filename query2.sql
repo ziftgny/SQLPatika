04030f3b267c2f2d4cce530e49b5ac789689dd53
ziftgny <114746717+ziftgny@users.noreply.github.com>
2023-07-27 12:40:21 +0300

    uploaded

--- query2.sql
+++ query2.sql
@@ -4,3 +4,11 @@ SELECT first_name,last_name FROM actor WHERE first_name IN ('Penelope','Nick','E
 --listed first name and last name columns from actor table where first name are penelope nick and ed
 SELECT * FROM film WHERE rental_rate IN (0.99, 2.99, 4.99) AND replacement_cost IN (12.99, 15.99, 28.99)
 --listed all columns from film table where rental rates are 0.99 2.99 and 4.99 and also replacement cost is 12.99 15.99 28.99
+SELECT * FROM film WHERE replacement_cost BETWEEN 12.99 AND 16.98;
+--listed all columns in film table where replacement cost is between 12.99 and 16.98
+
+SELECT first_name,last_name FROM actor WHERE first_name IN ('Penelope','Nick','Ed');
+--listed first name and last name columns from actor table where first name are penelope nick and ed
+
+SELECT * FROM film WHERE rental_rate IN (0.99, 2.99, 4.99) AND replacement_cost IN (12.99, 15.99, 28.99)
+--listed all columns from film table where rental rates are 0.99 2.99 and 4.99 and also replacement cost is 12.99 15.99 28.99
